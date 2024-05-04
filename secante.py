2b0fda2b37a37ecf569f8d8e29b36a65d8b11838
KarenS22 <karenquito2004@gmail.com>
2024-05-03 21:51:01 -0500

    Secante

--- secante.py
+++ secante.py
@@ -1,67 +1,109 @@
 import scipy.optimize as optimize
 import numpy as np
+import matplotlib.pyplot as plt
 import math
+import os
 
+x0= 0
+x1 = 1
+e = 0.01
+N = 10
 
 def f(x, function):
     return eval(function)
 
+
+def imprimir():
+    print("|------------------------------------------------|")
+    print("|            MÉTODO DE LA SECANTE                |")
+    print("|------------------------------------------------|")
+    print(f"| x0 = {x0}{(50-2-6-len(str(x0)))*" "}|")
+    print(f"| x1 = {x1}{(50-2-6-len(str(x1)))*" "}|")
+    print(f"| Error = {e}{(50-2-9-len(str(e)))*" "}|")
+    print(f"| Iteraciones maximas = {N}{(50-2-23-len(str(N)))*" "}|")
+    print("|------------------------------------------------|")
+
 def secante(function):
+        global x0, x1, e, N
+        imprimir()
+        v = input("DESEA EDITAR LOS VALORES INICIALES (s): ")
+        if(v.upper()=="S"):
+            os.system("cls")
+            imprimir()
+            x0 = float(input("Ingrese el límite inferior: "))
+            os.system("cls")
+            imprimir()
+            x1 = float(input("Ingrese el límite superior: "))
+            os.system("cls")
+            imprimir()
+            e = float(input("Ingrese el error: "))
+            os.system("cls")
+            imprimir()
+            N = int(input("Ingrese el número de iteraciones máximas: "))
+        os.system("cls")
+        imprimir()
 
-    x0 = float(input('Ingrese el primer número del intervalo: '))
-    x1 = float(input('Ingrese el segundo número del intervalo: '))
-    e = float(input('Máximo Error Tolerable: '))
-    N = int(input('Máximo Paso: '))
-    step_size = float(input('Tamaño de Paso: '))
+        # Encontrar la raíz en el intervalo [x0, x1]
+        root = optimize.root_scalar(lambda x: f(x, function), bracket=[x0, x1], method='brentq')
 
-
-
-    # Encontrar la raíz en el intervalo [x0, x1]
-    root = optimize.root_scalar(lambda x: f(x, function), bracket=[x0, x1], method='brentq')
-
-    print("\n   Raíz exacta:", round(root.root, 3))
-
-    print('\n\n*** METODO SECANTE ***')
-    condition = True
-    step = 1
-    while condition:
-        if f(x0, function) == f(x1, function):
-            print('División por cero, error!')
-            break
         
-        x2 = x0 - (x1 - x0) * f(x0, function) / (f(x1, function) - f(x0, function)) 
-        
-        fx2 = f(x2, function)
-        print('\n Iteración %d, x2 = %0.6f, f(x2) = %0.6f' % (step, x2, fx2), end=" ")
+        print("\n   Raíz exacta: ", root.root)
+        print()
 
-        # Calcular el error absoluto aproximado
-        if step > 1:
-            error_abs = abs(x2 - x_old)
-            et = (abs(root.root - x_old))/root.root
+        condition = True
+        step = 1
 
-            # Calcular el error aproximado porcentual (Et %)
-            error_aprox_porcentual = (error_abs / abs(x2)) * 100
-            print('Et:', et , end=" ")
-            print('Ea%:', round(error_aprox_porcentual, 6), end=" ")
+        print("Iteración      x          f(x)        Et%")
+        print("--------------------------------------------")
+        print("{:<12}{:<12.6f}{:<12.6f}".format(-1, x0, f(x0,function)))
+        print("{:<12}{:<12.6f}{:<12.6f}".format(0, x1, f(x1,function)))
 
-            if error_aprox_porcentual < e:
-                print('Convergencia alcanzada!')
+        while condition:
+            if f(x0, function) == f(x1, function):
+                print('División por cero, error!')
                 break
+            
+            x2 = x0 - (x1 - x0) * f(x0, function) / (f(x1, function) - f(x0, function)) 
+            
+            fx2 = f(x2, function)
+            
 
-        x_old = x2
-        x0 = x1
-        x1 = x2
-        step += 1
+            # Calcular el error absoluto aproximado
+
+            et = abs(root.root - x2)
+
+            etp = (et/root.root) *100
+
+            print("{:<12}{:<12.6f}{:<12.6f}{:<12.6f}".format(step, x2, fx2, etp))
+
+            x_old = x2
+            x0 = x1
+            x1 = x2
+            step += 1
+            
+            if step > N:
+                print('No Convergente!')
+                break
+            
+            condition = etp> e  
+        print('\n La raiz requerida es: %0.8f' % x2)
         
-        if step > N:
-            print('No Convergente!')
-            break
-        
-        condition = abs(f(x2, function)) > e
-    print('\n La raiz requerida es: %0.8f' % x2)
 
 
 
 
 def grafica(f):
+    global x0, x1, e, N
     print("Grafica")
+    x = np.linspace(x0, x1, 500)
+    try:
+        y = eval(f)
+    except:
+        print("Error: La expresión ingresada no es válida.")
+        exit()
+    plt.plot(x, y)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title('Gráfica de ' + f)
+    plt.grid(True)
+    plt.show()
