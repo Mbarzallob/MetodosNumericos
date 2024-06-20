0b38466146bb5d5ccc01f679b06a1557c73ec504
Mbarzallob <mbarzallob@hotmail.com>
2024-06-20 00:41:32 -0500

    Mejora en lagrange

--- lagrange.py
+++ lagrange.py
@@ -1,38 +1,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from tabulate import tabulate
+import getpass
+import os
+
+x_data = [1, 2, 3, 4, 5]
+y_data = [1, 4, 9, 16, 25]
+interpolacion = [1, 2, 3, 4, 5]
 
 def obtener_puntos():
-    puntos_x = []
-    puntos_y = []
-    print("Ingrese los puntos para la interpolación. Escriba 'done' cuando termine.")
-    while True:
-        x = input("x: ")
-        if x.lower() == 'done':
-            break
-        y = input("y: ")
-        if y.lower() == 'done':
-            break
-        try:
-            puntos_x.append(float(x))
-            puntos_y.append(float(y))
-        except ValueError:
-            print("Por favor, ingrese valores numéricos.")
-    return puntos_x, puntos_y
-
-def grafica(f):
-
-    puntos_x, puntos_y = obtener_puntos()
-
-    plt.scatter(puntos_x, puntos_y, color='red')
-    x = np.linspace(min(puntos_x), max(puntos_x), 500)
-    y = [lagrange_evaluar(p, puntos_x, puntos_y) for p in x]
-    plt.plot(x, y, label='Polinomio de Lagrange')
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.title('Interpolación de Lagrange')
-    plt.legend()
-    plt.grid(True)
-    plt.show()
+    global x_data, y_data, interpolacion
+    print("-------------------------------------------------")
+    print("         Interpolación de Lagrange")
+    print("-------------------------------------------------")
+    print("Existen los siguientes valores de prueba")
+    print("Datos del eje X:")
+    imprimirArray(x_data)
+    print("Datos del eje Y:")
+    imprimirArray(y_data)
+    print("Datos a interpolar:")
+    imprimirArray(interpolacion)
+    print("\n¿Desea cambiarlos?")
+    respuesta = int(input("1. Sí\n2. No\n"))
+    if respuesta == 1:
+        x_data = list(map(float, input("Ingrese los valores de x separados por comas: ").split(',')))
+        y_data = list(map(float, input("Ingrese los valores de y separados por comas: ").split(',')))
+        interpolacion = list(map(float, input("Ingrese los valores de interpolar separados por comas: ").split(',')))
+    else:
+        print("Datos no cambiados...")
+    os.system('cls' )
 
 def lagrange_evaluar(x, puntos_x, puntos_y):
     suma = 0
@@ -44,67 +40,61 @@ def lagrange_evaluar(x, puntos_x, puntos_y):
         suma += producto
     return suma
 
-def lagrange(f):
-    print("Seleccione el método de interpolación de Lagrange:")
-    print("1. Normal")
-    print("2. Inverso")
-    metodo = input("Opción: ")
+def grafica(f=None):
+    global x_data, y_data, interpolacion
+    x_fit = np.linspace(min(x_data), max(x_data), 500)
+    y_fit = [lagrange_evaluar(x, x_data, y_data) for x in x_fit]
+    
+    plt.scatter(x_data, y_data, label='Datos')
+    plt.plot(x_fit, y_fit, color='red', label='Interpolación de Lagrange')
+    
+    for i, txt in enumerate(y_data):
+        plt.annotate(f'{txt:.2f}', (x_data[i], y_data[i]), textcoords="offset points", xytext=(0, 10), ha='center')
+    
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.legend()
+    plt.show()
+    os.system('cls' )
 
-    if metodo not in ['1', '2']:
-        print("Opción no válida.")
-        return
+def imprimirArray(array):
+    print(", ".join(map(str, array)))
 
-    puntos_x, puntos_y = obtener_puntos()
-
-
-    print("Puntos dados:")
-    for i in range(len(puntos_x)):
-        print(f"({puntos_x[i]}, {puntos_y[i]})")
-
-    n = len(puntos_x)
-    polinomio = ""
-
-    if metodo == '1':  
-        for i in range(n):
-            L_i = f"L_{i}(x) = "
-            numerador = ""
-            denominador = ""
-            for j in range(n):
-                if i != j:
-                    numerador += f"(x - {puntos_x[j]})"
-                    denominador += f"({puntos_x[i]} - {puntos_x[j]})"
-                    if j != n - 1 and not (i == n - 2 and j == n - 1):
-                        numerador += " * "
-                        denominador += " * "
-            L_i += f"{numerador} / {denominador}"
-            print(L_i)
-
-            if i < n - 1:
-                polinomio += f"{puntos_y[i]} * {numerador} / {denominador} + "
-            else:
-                polinomio += f"{puntos_y[i]} * {numerador} / {denominador}"
-    else:  
-        for i in range(n):
-            L_i = f"L_{i}(x) = "
-            numerador = ""
-            denominador = ""
-            for j in range(n):
-                if i != j:
-                    numerador += f"(x - {puntos_y[j]})"
-                    denominador += f"({puntos_y[i]} - {puntos_y[j]})"
-                    if j != n - 1 and not (i == n - 2 and j == n - 1):
-                        numerador += " * "
-                        denominador += " * "
-            L_i += f"{numerador} / {denominador}"
-            print(L_i)
-
-            if i < n - 1:
-                polinomio += f"{puntos_x[i]} * {numerador} / {denominador} + "
-            else:
-                polinomio += f"{puntos_x[i]} * {numerador} / {denominador}"
-
+def imprimir_polinomio(polinomio):
     print("\nPolinomio de Lagrange:")
     print(f"P(x) = {polinomio}")
+
+def lagrange(f=None):
+    global x_data, y_data, interpolacion
+    os.system("cls")
+    obtener_puntos()
+
+    print("\nPuntos dados:")
+    print(tabulate(list(zip(x_data, y_data)), headers=['x', 'y'], tablefmt="grid"))
+
+    n = len(x_data)
+    polinomio = ""
+
+    for i in range(n):
+        L_i = f"L_{i}(x) = "
+        numerador = ""
+        denominador = ""
+        for j in range(n):
+            if i != j:
+                numerador += f"(x - {x_data[j]})"
+                denominador += f"({x_data[i]} - {x_data[j]})"
+                if j != n - 1 and not (i == n - 2 and j == n - 1):
+                    numerador += " * "
+                    denominador += " * "
+        L_i += f"{numerador} / {denominador}"
+        print(L_i)
+
+        if i < n - 1:
+            polinomio += f"{y_data[i]} * ({numerador} / {denominador}) + "
+        else:
+            polinomio += f"{y_data[i]} * ({numerador} / {denominador})"
+    
+    imprimir_polinomio(polinomio)
 
     while True:
         x = input("\nIngrese un valor de x para evaluar el polinomio (o 'salir' para terminar): ")
@@ -112,7 +102,9 @@ def lagrange(f):
             break
         try:
             x = float(x)
-            y = lagrange_evaluar(x, puntos_x, puntos_y)
+            y = lagrange_evaluar(x, x_data, y_data)
             print(f"P({x}) = {y}")
         except ValueError:
             print("Por favor, ingrese un número válido.")
+
+    os.system('cls' )
